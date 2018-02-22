bf70a8e40784b63b7afbb4ecdf03024a10d7de9d
zhaofinger <zhaohaibo1994@gmail.com>
2018-02-22 17:57:26 +0800

    chore: rollup 构建增加

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import { writeFileSync } from 'fs'
+import buble from 'rollup-plugin-buble'
+import vue from 'rollup-plugin-vue2'
+import css from 'rollup-plugin-css-only'
+import uglify from 'rollup-plugin-uglify'
+import { renderSync } from 'node-sass'
+import babel from 'rollup-plugin-babel'
+import { minify } from 'csso'
+
+const ENV = process.env.NODE_ENV
+const MIN = 'min.'
+const ENV_EXT = ENV === 'production' ? MIN : ''
+
+const plugins = [
+	vue({
+		autoStyles: false,
+		styleToImport: true
+	}),
+	css({
+		output(sass) {
+			// const css = renderSync({
+			// 	data: sass
+			// }).css.toString()
+			// const source = ENV === 'production' ? minify(css).css : css
+			// const path = `./dist/vue-z-toast.${ENV_EXT}css`
+
+			// writeFileSync(path, source)
+		}
+	}),
+	buble(),
+	babel({
+		exclude: 'node_modules/**' // 只编译我们的源代码
+	}),
+	uglify()
+]
+
+export default {
+	entry: './src/index.js',
+	plugins,
+	output: {
+		file: './dist/index.min.js',
+		format: 'umd',
+		name: 'vueZToast'
+	}
+}
