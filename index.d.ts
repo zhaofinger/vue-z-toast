8f988392868783323c0bcc18d348b5b5ff9d0ea7
zhaofinger <zhaohaibo1994@gmail.com>
2019-06-03 19:20:16 +0800

    [fix] types file fix

--- index.d.ts
+++ index.d.ts
@@ -7,21 +7,9 @@ export declare class ToastComponent extends Vue {
 	// close (): void
 }
 
-
-export interface ToastOptions {
-	message: string
-	duration?: number
-	styleObj?: Object
-}
-
-export interface Toast {
-	(text: string): ToastComponent
-	(options: ToastOptions): ToastComponent
-}
-
 declare module 'vue/types/vue' {
 	interface Vue {
 	/** Used to show feedback after an activity. The difference with Notification is that the latter is often used to show a system level passive notification. */
-		$toast: Toast
+		$toast(message: string, duration?: number, styleObj?: Object): ToastComponent
 	}
 }
